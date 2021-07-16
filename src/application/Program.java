4a65fb9d99ceee5767798aec7c7b68297de030d0
Rafael Santos <rafael_16001@hotmail.com>
2021-07-16 20:46:41 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,11 @@ public class Program {
 		sellerDao.insert(newSeller);	
 		System.out.println("Inserted! New id = " + newSeller.getId());
 
+		System.out.println("=== Test 5: seller update ======");
+		seller = sellerDao.findbyId(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("update completed");
 	}
 
 }
